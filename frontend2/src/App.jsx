3fa0459f45a8753f2f11cdf612d1378f971f1eeb
Deepak Poojary <63220831+deepakpoojary@users.noreply.github.com>
2024-05-22 13:56:45 +0530

    final

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -5,8 +5,20 @@ const App = () => {
   const [queryText, setQueryText] = useState("");
   const [response, setResponse] = useState("");
   const [chatbotOpen, setChatbotOpen] = useState(true);
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {
+      sender: "bot",
+      text: "Hello, Please choose any one",
+      // buttons: ["Option ", "Option 2", "Option 3"],
+    },
+    {
+      sender: "bot",
+      // text: "",
+      buttons: ["Soil report ", "suggestions", "Soil health card", "Questions"],
+    },
+  ]);
   const chatboxRef = useRef(null);
+  const inputRef = useRef(null);
 
   const toggleChatbot = () => {
     setChatbotOpen(!chatbotOpen);
@@ -52,6 +64,7 @@ const App = () => {
       };
       setMessages((prevMessages) => [...prevMessages, botMessage]);
     }
+    inputRef.current.focus();
   };
 
   useEffect(() => {
@@ -59,6 +72,43 @@ const App = () => {
       chatboxRef.current.scrollTop = chatboxRef.current.scrollHeight;
     }
   }, [messages]);
+  const handleButtonClick = async (option) => {
+    const userMessage = { sender: "user", text: option };
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    const requestBody = {
+      queryText: option,
+      sessionId: "abcd123",
+      languageCode: "en",
+    };
+    try {
+      const response = await fetch(
+        "https://dialogflow-dev.krishitantra.com/dialogflow",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not okkk");
+      }
+
+      const data = await response.text();
+      const botMessage = { sender: "bot", text: data };
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+      const botMessage = {
+        sender: "bot",
+        text: "Oops! Something went wrong. Please try again.",
+      };
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
+    }
+    inputRef.current.focus();
+  };
 
   return (
     <>
@@ -80,11 +130,30 @@ const App = () => {
                   <span className="material-symbols-outlined">
                     {message.sender === "user" ? "face" : "smart_toy"}
                   </span>
-                  <p>{message.text}</p>
+                  {/* above is just the icon */}
+                  {message.text && <p>{message.text}</p>}
+                  {message.buttons && (
+                    <div className="button-group">
+                      <br />
+                      {message.buttons.map((button, idx) => (
+                        <div key={idx}>
+                          <button
+                            key={idx}
+                            className="chat-button"
+                            onClick={() => handleButtonClick(button)}
+                          >
+                            {button}
+                          </button>
+                          <br />
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </li>
               ))}
             </ul>
           </div>
+          {/* chat input is perfect dont touch it  */}
           <div className="chat-input">
             <form className="chat-input" onSubmit={handleSubmit}>
               <input
@@ -92,6 +161,7 @@ const App = () => {
                 placeholder="Enter your message...."
                 value={queryText}
                 onChange={(e) => setQueryText(e.target.value)}
+                ref={inputRef}
               />
               <button
                 id="send-btn"
